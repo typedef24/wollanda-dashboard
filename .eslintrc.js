28c93c098f5cbb5d1cb406c0b9d251b66ecc4080
DesmondTambe <desking2000@gmail.com>
2020-09-07 00:58:02 +0100

    Eslint deleted from the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
-    page: true,
-    REACT_APP_ENV: true,
+    // ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
+    page: false,
+    REACT_APP_ENV: false,
+    
   },
 };
