d48ee8a6ce0af0fe721ef5a2063ce49dea932d63
DesmondTambe <desking2000@gmail.com>
2020-09-08 08:56:59 +0100

    Fresh commit, settings, dashboard, and transactions pages done.

--- src/pages/setting/components/input-field.jsx
+++ src/pages/setting/components/input-field.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Input } from 'antd';
 
@@ -13,19 +12,3 @@ function InputField(props) {
 }
 
 export default InputField;
-=======
-import React from 'react';
-import { Input } from 'antd';
-
-import Styles from '../style.less';
-
-function InputField(props) {
-  return (
-    <div>
-      <Input {...props} style={Styles.inputStyle} />
-    </div>
-  );
-}
-
-export default InputField;
->>>>>>> 581cc9a68afcf446e19171458be2654e50515279
